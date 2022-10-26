536b38b4899188abef4f693c1a993163530f67b4
GeronGitHub <geron65@hotmail.co.uk>
2022-10-26 10:34:36 +0100

    initial commit

---
+++ src/test/java/com/qa/foodordering/OnlineFoodOrderingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.foodordering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFoodOrderingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
