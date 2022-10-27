e00684229af6463dc4b283974df3bbc0416d0fc5
GeronGitHub <geron65@hotmail.co.uk>
2022-10-27 10:13:08 +0100

    validation

--- src/main/java/com/qa/foodordering/entity/Food.java
+++ src/main/java/com/qa/foodordering/entity/Food.java
@@ -6,6 +6,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -26,12 +30,20 @@ public class Food {
 	@Column(name = "food_id")
 	private int id;
 	
+	@NotNull
+	@Pattern(regexp = "^[A-Za-z]*", message = "Invalid category - must only contain alphabetical characters !!")
+	@Size(min = 2, max = 20, message = "Category must be between 2 and 20 characters long !!")
 	@Column(name = "food_category")
 	private String category; //CATEGORIES INCLUDE: CHICKEN, CHINESE, PIZZA, KEBAB, DESSERTS, ENGLISH
 	
+	@NotNull
+	@Pattern(regexp = "^[A-Za-z]*", message = "Invalid name - must only contain alphabetical characters !!")
+	@Size(min = 2, max = 20, message = "Name must be between 2 and 20 characters long !!")
 	@Column(name = "food_name")
 	private String name;
 	
+	@NotNull
+	@Min(0)
 	@Column(name = "food_price")
 	private double price;
 	
